7a78326afc370fabb95af796e5284817b0925666
marsvet <marsvet@163.com>
2019-08-05 01:44:39 +0800

    移动文件并稍作修改

--- app/app.py
+++ app/app.py
@@ -1,5 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template, request
 from py2neo import Graph, Node, Relationship, NodeMatcher
+import json
+import math
 
 app = Flask(__name__)
 
@@ -7,18 +9,25 @@ app = Flask(__name__)
 graph = Graph(
     "http://localhost:7474",
     username="neo4j",
-    password="******"
+    password="m1358044937"
 )
 
 
-@app.route('/events_intro/<page>', methods=['GET', 'POST'])
-def events_intro(page):  # 返回page页事件的简介信息
-    page = str(page)
-    data = graph.run('match (a:航空安全事件) match (a)-[:`包含`]->(b) return b.name skip ' + page + ' limit 30')
+@app.route("/")
+@app.route("/index")
+def index():
+    return render_template("index.html")
+
+
+@app.route('/all_events_intro') # 不写请求方式，默认为 get
+def all_events_intro():  # 返回page页事件的简介信息
+    per_page = 15   # 每页中包含的事件个数
+    page_num = math.ceil(graph.run('match (a:航空安全事件) match (a)-[:`包含`]->(b) return count(b)').data()[0]['count(b)'] / per_page) # 计算总页数，ceil 用于向上取整
+    page = request.args['page']     # get 请求的 url 类似这样：/events_intro?page=1。问号后面的参数会以字典的形式存放在 request.args 中。
+    data = graph.run("match (a:航空安全事件) match (a)-[:`包含`]->(b) return b.name skip %s limit %s" % (page, per_page))
     strdata = []
     for i in data:
         strdata.append('S' + str(i['b.name']))
-    print(strdata)
     events = []
     for name in strdata:
         one_info = {}
@@ -39,28 +48,28 @@ def events_intro(page):  # 返回page页事件的简介信息
         for data in event:
             one_info['航空公司'] = str(data['b.name'])
         events.append(one_info)
-    sort_events = sorted(events, key=lambda keys: keys['日期'])
-    return sort_events
+    sort_events = sorted(events, key=lambda keys: keys['日期'], reverse=True)
+    sort_events.append({'page_num':page_num})
+    return json.dumps(sort_events, ensure_ascii=False)  # 打包成 json 返回
 
 
-@app.route('/event_info', methods=['GET', 'POST'])
-def event_info(key, value):  # 返回指定属性的事件简介信息
+@app.route('/events_intro')
+def events_intro():  # 返回指定属性的事件简介信息
+    key = request.args['key']
+    value = request.args['value']
     find_node = graph.run('match (b:' + value + ') return b.name')
     for data in find_node:
         node = data['b.name']
     node = str(node)
     if key == '时间':
         node = 'T' + node
-    print(node)
     find_event = graph.run('match (a:' + node + ') match (a)<-[:属性]-(b) return b.name')
     events = []
     for data in find_event:
         events.append('S' + str(data['b.name']))
-    print(events)
     events_info = []
     for event in events:
         one_info = {}
-        print(event)
         event = str(event)
         one_info['事件名'] = event
         eventinfo = graph.run('match (a:' + event + ') match (a)-[:属性{name:"时间"}]->(b) return b.name')
@@ -79,20 +88,17 @@ def event_info(key, value):  # 返回指定属性的事件简介信息
         for data in eventinfo:
             one_info['航空公司'] = str(data['b.name'])
         events_info.append(one_info)
-    return events_info
+    return json.dumps(events_info, ensure_ascii=False)
 
 
-# event_info('航空公司', '天空服务航空公司')
-
-
-@app.route('/events_intro/<event_name>', methods=['GET', 'POST'])
-def events_intro(event_name):  # 根据事件名返回事件所有属性
+@app.route('/event_info')
+def event_info():  # 根据事件名返回事件所有属性
+    event_name = request.args['event_name']
     find_event = graph.run('match (b:' + event_name + ') return b.name')
     for data in find_event:
         node = data['b.name']
     node = str(node)
     event = 'S' + node
-    print(event)
     one_info = {}
     one_info['事件名'] = event
     eventinfo = graph.run('match (a:' + event + ') match (a)-[:属性{name:"时间"}]->(b) return b.name')
@@ -143,6 +149,4 @@ def events_intro(event_name):  # 根据事件名返回事件所有属性
     eventinfo = graph.run('match (a:' + event + ') match (a)-[:属性{name:"等级"}]->(b) return b.name')
     for data in eventinfo:
         one_info['等级'] = str(data['b.name'])
-    return one_info
-
-# events_intro('S0529JA848C')
+    return json.dumps(one_info, ensure_ascii=False)
