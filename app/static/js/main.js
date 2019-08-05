b46b1cdef9a1f0e4cc6e0f7e220320f8080372d1
marsvet <marsvet@163.com>
2019-08-05 17:17:44 +0800

    [Update] Complate event details page

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -2,9 +2,9 @@ function goToTop() {
 	window.scroll(0, 0);
 }
 
-function getLoadingHTML() {
+function getLoadingHTML(id) {
 	return `
-		<div id="loading">
+		<div id="${id}">
 			<div class="spinner-border text-warning" role="status"></div>
 			<span>Loading...</span>
 		</div>`
@@ -25,7 +25,7 @@ function showEventsCard(page, data) {
 					alt="${item['客机型号']}"
 				/>
 				<div class="card-body text-center">
-					<h2 class="card-title"><a href="#" class="text-success">${item["事件名"]}</a></h2>
+					<h2 class="card-title"><a class="text-success event-name" data-toggle="modal" data-target="#event-details">${item["事件名"]}</a></h2>
 					<div class="card-text">
 						<div class="row no-gutters">
 							<div class="col-6">日期：</div>
@@ -43,6 +43,7 @@ function showEventsCard(page, data) {
 				</div>
 			</div>`
 	$("main>div").html(toAddHtml);
+	$("a.event-name").on('click', showEventDetails);
 
 	const PAGER_BUTTON_NUM = 15
 	let toAddPager = '<li class="page-item"><div class="page-link">&laquo;</div></li>'
@@ -62,21 +63,38 @@ function showEventsCard(page, data) {
 	toAddPager += '<li class="page-item"><div class="page-link">&raquo;</div></li>'
 	$("#pager>ul").html(toAddPager);
 	$("#pager li").on('click', obj => {
-		if (obj.target.innerHTML == '«' || obj.target.innerHTML == '»' || obj.target.innerHTML == '...')
-			return;
-		$('main>div').html(getLoadingHTML);
-		$.get(`/all_events_intro?page=${obj.target.innerHTML}`, (data) => { showEventsCard(obj.target.innerHTML, data) });
+		if (obj.target.innerHTML != '«' && obj.target.innerHTML != '»' && obj.target.innerHTML != '...') {
+			$('main>div').html(getLoadingHTML('loading-events-card'));
+			$('#pager>ul').html('')
+			$.get(`/all_events_intro?page=${obj.target.innerHTML}`, (data) => { showEventsCard(obj.target.innerHTML, data) });
+		}
 	})
 	$("#pager li:first-child").on('click', () => {
 		if (page > 1) {
-			$('main>div').html(getLoadingHTML);
+			$('main>div').html(getLoadingHTML('loading-events-card'));
+			$('#pager>ul').html('')
 			$.get(`/all_events_intro?page=${Number(page) - 1}`, (data) => { showEventsCard(Number(page) - 1, data) });
 		}
 	})
 	$("#pager li:last-child").on('click', () => {
 		if (page < page_num) {
-			$('main>div').html(getLoadingHTML);
+			$('main>div').html(getLoadingHTML('loading-events-card'));
+			$('#pager>ul').html('')
 			$.get(`/all_events_intro?page=${Number(page) + 1}`, (data) => { showEventsCard(Number(page) + 1, data) });
 		}
+	})
+}
+
+function showEventDetails(obj) {
+	let title = obj.target.innerHTML;
+	$("#event-details .modal-title").html(title);
+	$("#event-details .modal-body").html(getLoadingHTML('loading-event-details'));
+	$.get(`event_info?event_name=${obj.target.innerHTML}`, data => {
+		let jsonObj = JSON.parse(data);
+		let toAddHTML = '<table class="table table-striped table-hover"><tbody>'
+		for (let i in jsonObj)
+			toAddHTML += `<tr><th>${i}</th><td>${jsonObj[i]}</td></tr>`;
+		toAddHTML += '</tbody></table>';
+		$("#event-details .modal-body").html(toAddHTML);
 	})
 }
