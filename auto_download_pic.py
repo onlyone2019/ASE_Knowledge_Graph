b95718a9397ba371ac3d14e3534508181d2ed0ce
marsvet <marsvet@163.com>
2019-08-11 09:08:54 +0800

    [Update] auto_download_pic.py

--- auto_download_pic.py
+++ auto_download_pic.py
@@ -21,23 +21,21 @@ class Pic_spider:
 
     def get_picture(self, aircraft):
         sleep(2)
-        element = self.driver.find_element_by_xpath('//*[@id="sbtc"]/div/div[1]/input')
+        element = self.driver.find_element_by_css_selector('[id="sbtc"] input')
         element.clear()
         element.send_keys(aircraft + '\n')
 
         sleep(2)
         try:
             pictures = self.driver.find_element_by_xpath('//*[@id="rg"]').find_elements_by_tag_name('img')
+            for pic in pictures:
+                image = Image.open(BytesIO(b64decode(pic.get_attribute('src').split(',')[-1].replace('%0A', '\n'))))
+                pic_width, pic_height = image.size
+                if pic_width / pic_height > 1.5:
+                    image.save('%s.jpeg' % aircraft)
+                    return 1
         except:
             return 0
-        for pic in pictures:
-            image = Image.open(BytesIO(b64decode(pic.get_attribute('src').split(',')[-1].replace('%0A', '\n'))))
-            pic_width, pic_height = image.size
-            if pic_width / pic_height > 1.5:
-                image.save('%s.jpeg' % aircraft)
-                return 1
-
-        return 0
 
     def quit(self):
         self.driver.quit()
@@ -45,23 +43,25 @@ class Pic_spider:
 
 if __name__ == "__main__":
     try:
-        os.mkdir('images')
+        os.mkdir('aircraft_images')
     except:
         pass
     with open('客机型号.txt', 'r') as f:
-        to_download = set(f.readlines())
-    download_fail = []
-    os.chdir('images')
+        to_download = {i[:-1] for i in f.readlines()}   # 集合推导式，去除重复项和行尾换行符
+    os.chdir('aircraft_images')
+    already_exist = os.listdir()
 
     pic_spider = Pic_spider()
     pic_spider.startup()
 
+    with open('../下载失败.txt', 'w') as f:
+        f.write('下载失败：\n')
     for aircraft in to_download:
+        if aircraft + '.jpeg' in already_exist:
+            print(aircraft + '.jpeg already existed.')
+            continue
         if not pic_spider.get_picture(aircraft):
-            download_fail.append(aircraft)
+            with open('../下载失败.txt', 'a') as f:
+                f.write(aircraft + '\n')
 
     pic_spider.quit()
-
-    print('\n下载失败：')
-    for i in download_fail:
-        print(i + '\n')
