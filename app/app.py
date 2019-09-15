0bc52778abd3e5d82ef6922bb639336a00296661
onlyone2019 <2332642930@qq.com>
2019-09-15 10:50:42 +0800

    add pattern_bottom

--- app/app.py
+++ app/app.py
@@ -258,3 +258,15 @@ def get_reason_participle():  #返回原因的分词
                 result[detail] += 1
     result = sorted(result.items(), key=lambda x: x[1], reverse=False)
     return jsonify(result)
+
+@app.route('/pattern_bottom')
+def get_pattern_bottom():
+    result = []
+    find_names = graph.run("match (a:模式) where a.grade=2 return a.name")
+    for data in find_names:
+        s = str(data['a.name'])
+        find_child = graph.run("match (a:模式) where a.name='" + s + "' match (a)-[r]->(b) where exists(b.name) return a.name limit 1")
+        for detail in find_child:
+            result.append(detail['a.name'])
+    result = list(set(result))
+    return jsonify(result)
